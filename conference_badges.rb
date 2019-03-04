389f6eaa218f9168d17b7a944cce0ad1a9901902
Tommi Cox <tommicox@gmail.com>
2019-03-04 04:07:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
 attendees = ["Edsger", "Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
-def badge_maker("Arel")
-  puts "Hello, my name is Arel."
+def badge_maker(name)
+  puts "Hello, my name is ."
 end

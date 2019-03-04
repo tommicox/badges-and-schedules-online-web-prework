debbd643029cac6a6d5eaa9a09eafb81cc06e4c2
Tommi Cox <tommicox@gmail.com>
2019-03-04 04:16:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,10 @@
 # Write your code here.
 attendees = ["Edsger", "Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
  "Hello, my name is #{name}."
 end
+
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
   "Hello, my name is #{attendee}."

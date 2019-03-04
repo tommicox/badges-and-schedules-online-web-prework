980f2247e9be0e501c73b02c363d62386b56ddfb
Tommi Cox <tommicox@gmail.com>
2019-03-04 04:25:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,9 @@ def batch_badge_creator(attendees)
   "Hello, my name is #{attendee}."
 end
 end
+
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-  "Hello #{attendee}! You'll be assigned to room #{index+1}!"
+  "Hello #{attendee}! You'll be assigned to room #{index + 1}!"
 end
 end
